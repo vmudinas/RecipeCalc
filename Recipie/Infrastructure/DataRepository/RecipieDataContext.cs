29fb5a940ceb3b059721955a7f25c04c76a046ba
Vytautas M <gliukaz@gmail.com>
2016-08-31 07:22:47 -0700

    css and validation added for html

---
+++ Recipie/Infrastructure/DataRepository/RecipieDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interface;
+using System.Data.Entity;
+
+namespace DataRepository
+{
+    public class RecipieRepoDataContext : DbContext
+    {
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Recipe> Recipies { get; set; }
+
+        public DbSet<RecipeTotals> RecipeTotals { get; set; }
+
+
+    }
+}
